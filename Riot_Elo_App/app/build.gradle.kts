e23c968f6aad6f56059a39f6d72daa9c35ba5e59
Bui-Duc-Phu <buiphu05012002@gmail.com>
2025-05-08 17:59:57 +0700

    update library

--- Riot_Elo_App/app/build.gradle.kts
+++ Riot_Elo_App/app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    //thêm kapt sau đó sync
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+
 }
 
 android {
@@ -56,4 +61,33 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    // Ktor client dependencies
+    implementation("io.ktor:ktor-client-core:2.3.0")
+    implementation("io.ktor:ktor-client-android:2.3.0")
+    implementation("io.ktor:ktor-client-json:2.3.0")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
+
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
+
+    //viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+
+
+    //navigation
+    implementation("androidx.navigation:navigation-compose:2.8.7")
+
+
+
+
 }
---
+++ Riot_Elo_App/app/src/main/java/com/example/riot_elo/ui/screem/detailUserScreen/DetailUserScreen.kt
@@ -0,0 +1,2 @@
+package com.example.riot_elo.ui.screem.detailUserScreen
+
